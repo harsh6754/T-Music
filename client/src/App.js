ee09bb4a49f6f99a3bd91f008a77c1c4e9e765cc
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-09 08:50:05 +0530

    T-Music

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+    return (
+        <div className='
+            w-screen
+            h-screen
+            bg-blue-400 
+            flex justify-center 
+            items-center'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Routes>
+                <Route path='/login' />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
