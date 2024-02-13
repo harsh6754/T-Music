27113631db361ee7e877a131558a15e13c248003
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-13 08:46:07 +0530

    T-Music

--- server/app.js
+++ server/app.js
@@ -1,11 +1,30 @@
 const express = require("express")
 const app = express()
 
+require("dotenv/config")
+
 const cors = require("cors")
+const {default : mongoose, Mongoose} = require("mongoose");
+
+
+// app.use(cors({origin : true}));
+
 
 app.get("/",(req,res)=>{
     return res.json("Hii Baby....")
 })
+
+mongoose.connect(process.env.DB_STRING,{
+    useNewUrlParser : true
+})
+mongoose.connection
+.once("open",()=>{
+    console.log("MongoDB is connected");
+})
+.on("error",(error)=>{
+    console.log(`ERROR : ${error}`);
+})
+
 
 app.listen(4000,()=>{
     console.log("Listening to port 4000")
