ed9880fd6a92b2992faeb4b4fa8f1ef5e4042069
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-13 15:52:31 +0530

    T-Music

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -22,12 +22,36 @@ router.get("/login", async (req, res) => {
             //return res.status(200).json(decodeValue)
 
             //Check User exit or Not
-
+            const userExists = await user.findOne({"user_id" : decodeValue.user_id})
+            if(!userExists){
+                //return res.send("Need to create")
+                newUserData(decodeValue,req,res)
+            }else{
+                return res.send("Need to Update")
+            }
         }
     } catch (error) {
         return res.status(505).json({ message: error })
     }
 
 })
+
+const newUserData = async (decodeValue,req,res)=>{
+    const newUser = new user({
+        name:decodeValue.name,
+        email:decodeValue.email,
+        imageURL:decodeValue.picture,
+        user_id:decodeValue.user_id,
+        email_verified:decodeValue.email_verified,
+        role:"member",
+        auth_time:decodeValue.auth_time
+    })
+    try {
+        const savedUser = await newUser.save()
+        res.status(200).send({user:savedUser})
+    } catch (error) {
+         res.status(400).send({success : false, msg : error})
+    }
+}
 
 module.exports = router;
