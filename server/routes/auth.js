b2a5f03684264be51ff46ccaba9da609106f1b9e
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-13 13:09:28 +0530

    T-Music

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,8 @@
 const router = require("express").Router()
 
+const user = require("../models/user")
+
+
 const admin = require("../config/firebase.config")
 
 
@@ -16,7 +19,10 @@ router.get("/login", async (req, res) => {
         if (!decodeValue) {
             return res.status(505).json({ message: "Un-Authorized"})
         }else{
-            return res.status(200).json(decodeValue)
+            //return res.status(200).json(decodeValue)
+
+            //Check User exit or Not
+
         }
     } catch (error) {
         return res.status(505).json({ message: error })
