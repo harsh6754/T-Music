96246a80fc93dab06e7359a86001ba46ff54ec54
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-13 11:34:09 +0530

    T-Music

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,14 @@
 const router = require("express").Router()
 
-router.get("/login", (req,res) => {
-    return res.json("Successfully Login with Google")
+router.get("/login", (req, res) => {
+    //return res.json("Successfully Login with Google")
+    //return res.send(req.headers.authorization)
+    if (!req.headers.authorization) {
+        return res.status(500).send({ message: "Invalid Token" })
+    }
+    const token = req.headers.authorization.split(" ")[1];
+    //return res.send(token);
+    
 })
 
 module.exports = router;
