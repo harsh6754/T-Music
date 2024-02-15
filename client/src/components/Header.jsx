620d1f925fbd3d0f390b5957ce0ff148b4df7b5b
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-15 10:28:34 +0530

    Logout funtionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,30 @@ import { Logo } from '../assets/img/index'
 import { NavLink } from 'react-router-dom'
 
 import { isActiveStyles, isNotActiveStyles } from '../utils/styles'
-import { FaCrown, FaUser } from 'react-icons/fa'
+import { FaCrown } from 'react-icons/fa'
+//import { AiOutlineUser, AiOutlineHeart, AiOutlineLogout } from 'react-icons/ai';
+import { FaUser } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
+import { IoMdLogOut } from "react-icons/io";
+
 
 import { useStateValue } from '../context/StateProvider'
+//import {getApp} from 'firebase/app'
+//import {firebaseAuth} from '../../../server/config/firebase.config'
+import { getAuth } from 'firebase/auth'
+import { app } from "../config/firebase.config"
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
     const [{ user }, dispatch] = useStateValue();
+    const navigate = useNavigate();
+    const logOut = () => {
+        const firebaseAuth = getAuth(app)
+        firebaseAuth.signOut().then(() => {
+            window.localStorage.setItem("auth", "false")
+        }).catch((e) => console.log(e));
+        navigate("/login", { replace: true })
+    }
     return (
         <header className='flex items-center w-full p-4 md:py-2 md:px-6'>
             <NavLink to={"/"}>
@@ -57,14 +75,20 @@ const Header = () => {
                     <p className='text-textColor text-lg hover:text-headingColor font-semibold'>{user?.user?.name}</p>
                     <p className='flex items-center gap-2 text-xs text-gray-500 font-normal'>Premium Member <FaCrown className="text-sm -ml-1 text-yellow-500" /></p>
                 </div>
-                <div className=" absolute z-10 top-12 right-0 w-275 gap-2 p-3 bg-card shadow-lg rounded-lg backdrop-blur-sm flex flex-col">
-                    <NavLink to={'/userProfile'}>
+                <div className="absolute z-10 top-12 right-0 w-275 gap-2 p-3 bg-card shadow-lg rounded-lg backdrop-blur-sm flex flex-col">
+                    <NavLink to={'/userProfile'} className='flex items-center'>
+                        <FaUser size={20} className='mr-2' />
                         <p className='text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out'>Profile</p>
-
                     </NavLink>
-                    <p className='text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out'>My Favourites</p>
+                    <p className='text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out flex items-center'>
+                        <MdFavorite size={20} className='mr-2' />
+                        My Favourites
+                    </p>
                     <hr />
-                    <p className='text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out'>Logout</p>
+                    <p className='text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out flex items-center' onClick={logOut}>
+                        <IoMdLogOut size={20} className='mr-2' />
+                        Sign Out
+                    </p>
                 </div>
             </div>
         </header>
