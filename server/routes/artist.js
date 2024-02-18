dbebb4f4e352070b76df51825832f770aadb8135
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-18 14:33:56 +0530

    Artist Routes Finished

--- server/routes/artist.js
+++ server/routes/artist.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const artist = require("../models/artist");
 
+//All Artist Save
 router.post("/save", async (req, res) => {
    const { name, imageURL, twitter, instagram } = req.body;
 
@@ -19,6 +20,7 @@ router.post("/save", async (req, res) => {
    }
 });
 
+// Get Only One Artist Data
 router.get("/getOne/:id", async (req, res) => {
    try {
       const foundArtist = await artist.findById(req.params.id);
@@ -34,6 +36,7 @@ router.get("/getOne/:id", async (req, res) => {
    }
 });
 
+//Get All Artist Data
 router.get("/getAll", async (req, res) => {
    try {
       const allArtists = await artist.find().sort({ createdAt: 1 });
@@ -41,6 +44,47 @@ router.get("/getAll", async (req, res) => {
       return res.status(200).json({ success: true, artists: allArtists });
    } catch (error) {
       console.error("Error fetching artists:", error);
+      return res.status(500).json({ success: false, msg: "Internal server error" });
+   }
+});
+
+//Delete Artist Data
+router.delete("/delete/:id", async (req, res) => {
+   try {
+      const filter = { _id: req.params.id }; // Corrected accessing params
+      const result = await artist.deleteOne(filter);
+
+      if (result.deletedCount > 0) { // Check if any document was deleted
+         return res.status(200).send({ success: true, msg: "Data Deleted Successfully" });
+      } else {
+         return res.status(404).send({ success: false, msg: "Data Not Found" });
+      }
+   } catch (error) {
+      console.error("Error:", error);
+      return res.status(500).send({ success: false, msg: "Internal Server Error" });
+   }
+});
+
+//Update Artist Data
+router.put("/update/:id", async (req, res) => {
+   const artistId = req.params.id;
+   const { name, imageURL, twitter, instagram } = req.body;
+
+   try {
+      const updatedArtist = await artist.findByIdAndUpdate(artistId, {
+         name,
+         imageURL,
+         twitter,
+         instagram
+      }, { new: true });
+
+      if (updatedArtist) {
+         return res.status(200).json({ success: true, artist: updatedArtist});
+      } else {
+         return res.status(404).json({ success: false, msg: "Artist not found" });
+      }
+   } catch (error) {
+      console.error("Error updating artist:", error);
       return res.status(500).json({ success: false, msg: "Internal server error" });
    }
 });
