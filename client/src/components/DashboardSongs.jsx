2bf4fe2ceeabe9aca9f0f0dcacaadbf1fc1acd37
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-24 11:13:45 +0530

    Songs Dashboard

--- client/src/components/DashboardSongs.jsx
+++ client/src/components/DashboardSongs.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { IoAdd, IoPause, IoPlay, IoTrash } from 'react-icons/io5';
 import { AiOutlineClear } from 'react-icons/ai';
+import { useStateValue } from '../context/StateProvider';
+import { getAllSongs } from '../api';
+import { actionType } from '../context/reducer';
+import  SongCard  from './SongCard'
 
 const DashboardSongs = () => {
   const [songFilter, setSongFilter] = useState("");
   const [isFocus, setIsFocus] = useState(false);
+  const [{ allSongs }, dispatch] = useStateValue()
+
+  useEffect(() => {
+    if (!allSongs) {
+      getAllSongs().then(data => {
+        dispatch({
+          type: actionType.SET_ALL_SONGS,
+          allSongs: data.allSongs
+        })
+      })
+    }
+  }, [])
 
   return (
     <div className='w-full p-4 flex items-center justify-center flex-col'>
@@ -29,10 +45,31 @@ const DashboardSongs = () => {
         />
 
         <i >
-          <AiOutlineClear className='text-3xl text-textColor cursor-pointer'/>
+          <AiOutlineClear className='text-3xl text-textColor cursor-pointer' />
         </i>
-
       </div>
+
+      {/*Main Comtainer */}
+      <div className='relative w-full my-4 p-4 border border-gray-300 rounded-md'>
+        {/* Count */}
+        <div className='absolute top-4 left-4'>
+          <p className='text-xl font-bold'>
+            <span className='text-sm font-semibold text-textColor'>Count :{""} </span>
+            {allSongs?.length}
+          </p>
+        </div>
+        <SongContainer data={allSongs} />
+      </div>
+    </div>
+  )
+}
+
+export const SongContainer = ({ data }) => {
+  return (
+    <div className="w-full flex flex-wrap gap-3 items-center justify-evenly">
+     {data && data.map((song,i) => 
+    <SongCard key={song._id} data={song} index={i}/>
+     )}
     </div>
   )
 }
---
+++ client/src/components/SongCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SongCard = ({data,index}) => {
+  return (
+    <div>
+      SongCard
+    </div>
+  )
+}
+
+export default SongCard
