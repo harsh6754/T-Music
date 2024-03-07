284dd6ed4475101e8bc723ca47cab4a1c78410ef
harsh6754 <210306105012@paruluniversity.ac.in>
2024-03-07 08:33:44 +0530

    Statics Page Design

--- client/src/Statics/DownloadAds.jsx
+++ client/src/Statics/DownloadAds.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function DownloadAds() {
-    const downloadImgStyle = 'border-[2px] border-[#232A4E] rounded-[13px] h-[3rem] w-[10rem]'
+    const downloadImgStyle = 'border-[2px] border-[#232A4E] rounded-[13px] h-[3rem] w-[10rem] cursor-pointer'
   return (
     <div className="download">
         <div className="download_images flex">
